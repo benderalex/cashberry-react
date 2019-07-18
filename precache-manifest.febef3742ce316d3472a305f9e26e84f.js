b364e681b07798a77914f0be01ca0e1e0948a198
benderalex5 <benderalex5@gmail.com>
2019-07-18 11:08:52 +0300

    Updates

---
+++ precache-manifest.febef3742ce316d3472a305f9e26e84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac146a6d6a943d0eb9e71c59ae46a51b",
+    "url": "/cashberry-react/index.html"
+  },
+  {
+    "revision": "672acae0688119443558",
+    "url": "/cashberry-react/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "672acae0688119443558",
+    "url": "/cashberry-react/static/js/2.5a93a0d9.chunk.js"
+  },
+  {
+    "revision": "576b4b2a01bd6865de29",
+    "url": "/cashberry-react/static/js/main.e858fbe8.chunk.js"
+  },
+  {
+    "revision": "654a81e9501964c88087",
+    "url": "/cashberry-react/static/js/runtime~main.c4b8c9d8.js"
+  }
+]);
